a35402e0e421e0a258d1add4cf95fe793a86b729
OmShirke <113794365+OmShirke@users.noreply.github.com>
2024-02-18 21:22:19 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Wrapper } from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <div className="App">
+        <header>CALCULATOR</header>
+      </div>
+    </Wrapper>
   );
 }
+
+export const ButtonBox = ({ children }) => {
+  return <div className="buttonBox">{children}</div>;
+};
 
 export default App;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,5 @@
+import "./components.css";
+
+export const Wrapper = ({ children }) => {
+  return <div className="wrapper">{children}</div>;
+};
